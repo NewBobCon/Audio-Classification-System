3c839f92c6393b90e373453aa0e12aef4fa1fb35
Conor McIntosh <mcintc2@uw.edu>
2022-03-06 15:58:17 -0800

    -Added label to clarify what the check buttons do -Allow users to check which files to include for making the model
    Co-authored-by: michzelinger <zelinger.mich@gmail.com>
    Co-authored-by: kangyul <kangyul@users.noreply.github.com>
    Co-authored-by: Benjamin Olson <BenjaminOlsonCS@users.noreply.github.com>

--- FileViewer.py
+++ FileViewer.py
@@ -4,9 +4,9 @@
 #machine learing algorithm in 'ph_ml_algo.py'
 
 from tkinter import *
-#from tkinter import filedialog
 import glob, math, os, re
 import statistics
+from tkinter.scrolledtext import ScrolledText
 
 class FileViewer(Frame):
 
@@ -17,14 +17,15 @@ class FileViewer(Frame):
         self.musicFiles = []
         self.resultWin = resultWin
         self.nums = re.compile(r'(\d+)')
+        self.modelBooleans = []
 
         #Create main frame
         mainFrame = Frame(master)
         mainFrame.pack()
 
         #Create File List frame
-        listFrame = Frame(mainFrame)
-        listFrame.pack(side=TOP)
+        self.listFrame = Frame(mainFrame)
+        self.listFrame.pack(side=TOP)
 
         #Create Compute frame
         computeFrame = Frame(mainFrame)
@@ -38,33 +39,53 @@ class FileViewer(Frame):
         resultsControl.pack(side=BOTTOM)
 
         #Create Buttons
-        fileSelector = Button(listFrame, text="File Select", fg="black", padx=10, width=10, command=lambda: self.get_audio_files())
-        fileSelector.pack(side=TOP)
+        fileSelector = Button(self.listFrame, text="Pull Files", fg="black", padx=10, width=10, command=lambda: self.get_audio_files())
+        fileSelector.pack(side=BOTTOM)
         compAudio = Button(computeFrame, text="Compute", fg="green", padx=10, width=10, command=lambda: self.comp_audio())
         compAudio.pack(side=BOTTOM)
 
+        l1 = Label(self.listFrame, text='Check the files to \nmake the model with: ', fg='red')
+        l1.pack(side=LEFT)
+
         #Create File Listbox
-        self.listScrollbar = Scrollbar(listFrame)
-        self.list = Listbox(listFrame, yscrollcommand=self.listScrollbar.set, selectmode=BROWSE, height=20)
-
+        self.listScrollbar = Scrollbar(self.listFrame)
+        #self.filelist = Listbox(self.listFrame, yscrollcommand=self.listScrollbar.set, selectmode=BROWSE, height=20, width=40)
+        self.filelist = ScrolledText(self.listFrame, height=20, width=40)
+        self.filelist.pack(side=LEFT)
         
-
     def fileSort(self, num):
         parts = self.nums.split(num)
         parts[1::2] = map(int, parts[1::2])
         return parts
-        
-    
+
+    def listbar_helper(self, fileListP, startPoint):
+        for i in range(len(fileListP)):
+            self.modelBooleans.append(IntVar(0))
+            cb = Checkbutton(self.filelist, text=fileListP[i], variable=self.modelBooleans[i + startPoint], onvalue=1, offvalue=0)
+            self.filelist.window_create('end', window=cb)
+            self.filelist.insert('end', '\n')
+
     def build_listbar(self):
-        for i in range(len(self.speechFiles)):
-            self.list.insert(i, self.speechFiles[i])
-        for j in range(len(self.musicFiles)):
-            self.list.insert(j + len(self.speechFiles), self.musicFiles[j])
-        self.list.pack(side=LEFT, fill=BOTH)
-        self.list.activate(1)
-        self.listScrollbar.config(command=self.list.yview)
+        self.listbar_helper(self.speechFiles, 0)
+        self.listbar_helper(self.musicFiles, len(self.speechFiles))
+        # for i in range(len(self.speechFiles)):
+        #     #self.filelist.insert(i, self.speechFiles[i])
+        #     self.modelBooleans.append(IntVar(0))
+        #     cb = Checkbutton(self.filelist, text=self.speechFiles[i], variable=self.modelBooleans[i], onvalue=1, offvalue=0)
+        #     self.filelist.window_create('end', window=cb)
+        #     self.filelist.insert('end', '\n')
+        # for j in range(len(self.musicFiles)):
+        #     #self.filelist.insert(j + len(self.speechFiles), self.musicFiles[j])
+        #     cb = Checkbutton(self.filelist, text=self.musicFiles[j], variable=self.modelBooleans[j], onvalue=1, offvalue=0)
+        #     self.filelist.window_create('end', window=cb)
+        #     self.filelist.insert('end', '\n')
+        self.filelist.pack(side=LEFT, fill=BOTH)
+        self.listScrollbar.config(command=self.filelist.yview)
 
     def get_audio_files(self):
+        self.filelist.delete("1.0","end")
+        self.musicFiles.clear()
+        self.speechFiles.clear()
         for infile in sorted(glob.glob('audio/music/*.wav'), key=self.fileSort):
             file, ext = os.path.split(infile)
             self.musicFiles.append(ext)
@@ -74,10 +95,6 @@ class FileViewer(Frame):
         print(self.musicFiles)
         print(self.speechFiles)
         self.build_listbar()
-        # filetypes = (('audio files', '*.wav'), ('All files', '*.*'))
-        # filenames = filedialog.askopenfilenames(title='Open Files', initialdir='/', filetypes=filetypes)
-        # print(filenames)
-        # showinfo(title='Selected Files', message=filenames)
         return
         
 
