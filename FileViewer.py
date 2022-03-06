13370723704f6e54e9f4cb00038dee6cbe4362e3
Conor McIntosh <mcintc2@uw.edu>
2022-03-06 14:10:29 -0800

    -Added test audio file -Added listbar for the audio files -Can now open audio files into the listbar Co-authored-by: michzelinger <zelinger.mich@gmail.com> Co-authored-by: kangyul <kangyul@users.noreply.github.com> Co-authored-by: Benjamin Olson <BenjaminOlsonCS@users.noreply.github.com>

--- FileViewer.py
+++ FileViewer.py
@@ -4,19 +4,25 @@
 #machine learing algorithm in 'ph_ml_algo.py'
 
 from tkinter import *
+#from tkinter import filedialog
+import glob, math, os, re
+import statistics
 
 class FileViewer(Frame):
 
     def __init__(self, master, resultWin):
         Frame.__init__(self, master)
         self.master = master
+        self.speechFiles = [] 
+        self.musicFiles = []
         self.resultWin = resultWin
+        self.nums = re.compile(r'(\d+)')
 
         #Create main frame
         mainFrame = Frame(master)
         mainFrame.pack()
 
-        #Create List frame
+        #Create File List frame
         listFrame = Frame(mainFrame)
         listFrame.pack(side=TOP)
 
@@ -39,8 +45,40 @@ class FileViewer(Frame):
 
         #Create File Listbox
         self.listScrollbar = Scrollbar(listFrame)
-        self.listScrollbar.pack(side=RIGHT, fill=Y)
         self.list = Listbox(listFrame, yscrollcommand=self.listScrollbar.set, selectmode=BROWSE, height=20)
+
+        
+
+    def fileSort(self, num):
+        parts = self.nums.split(num)
+        parts[1::2] = map(int, parts[1::2])
+        return parts
+        
+    
+    def build_listbar(self):
+        for i in range(len(self.speechFiles)):
+            self.list.insert(i, self.speechFiles[i])
+        for j in range(len(self.musicFiles)):
+            self.list.insert(j + len(self.speechFiles), self.musicFiles[j])
+        self.list.pack(side=LEFT, fill=BOTH)
+        self.list.activate(1)
+        self.listScrollbar.config(command=self.list.yview)
+
+    def get_audio_files(self):
+        for infile in sorted(glob.glob('audio/music/*.wav'), key=self.fileSort):
+            file, ext = os.path.split(infile)
+            self.musicFiles.append(ext)
+        for infile in sorted(glob.glob('audio/speech/*.wav'), key=self.fileSort):
+            file, ext = os.path.split(infile)
+            self.speechFiles.append(ext)
+        print(self.musicFiles)
+        print(self.speechFiles)
+        self.build_listbar()
+        # filetypes = (('audio files', '*.wav'), ('All files', '*.*'))
+        # filenames = filedialog.askopenfilenames(title='Open Files', initialdir='/', filetypes=filetypes)
+        # print(filenames)
+        # showinfo(title='Selected Files', message=filenames)
+        return
         
 
 if __name__ == '__main__':
