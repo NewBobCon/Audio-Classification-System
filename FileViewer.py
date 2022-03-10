4fe0115352526abf3b81ac598180f7455aaa7cfc
Conor McIntosh <mcintc2@uw.edu>
2022-03-09 22:57:40 -0800

    -Switched to using Canvas to scroll between files -Added "play" button feature to each file that appears Co-authored-by: kangyul <kangyul@users.noreply.github.com> Co-authored-by: michzelinger <zelinger.mich@gmail.com> Co-authored-by: Benjamin Olson <BenjaminOlsonCS@users.noreply.github.com>

--- FileViewer.py
+++ FileViewer.py
@@ -4,9 +4,10 @@
 #machine learing algorithm in 'ph_ml_algo.py'
 
 from tkinter import *
-import glob, math, os, re
-import statistics
+import glob, math, os, re, sys, subprocess, statistics
 from tkinter.scrolledtext import ScrolledText
+from tkinter import filedialog
+from typing import IO
 
 class FileViewer(Frame):
 
@@ -18,6 +19,8 @@ class FileViewer(Frame):
         self.resultWin = resultWin
         self.nums = re.compile(r'(\d+)')
         self.modelBooleans = []
+        self.xmax = 150
+        self.ymax = 30
 
         #Create main frame
         mainFrame = Frame(master)
@@ -34,7 +37,7 @@ class FileViewer(Frame):
         #Create Results frame
         resultFrame = Frame(self.resultWin)
         resultFrame.pack(side=TOP)
-        self.canvas = Canvas(resultFrame)
+        self.filelist = Canvas(resultFrame)
         resultsControl = Frame(resultFrame)
         resultsControl.pack(side=BOTTOM)
 
@@ -48,39 +51,70 @@ class FileViewer(Frame):
         l1.pack(side=LEFT)
 
         #Create File Listbox
-        self.listScrollbar = Scrollbar(self.listFrame)
         #self.filelist = Listbox(self.listFrame, yscrollcommand=self.listScrollbar.set, selectmode=BROWSE, height=20, width=40)
-        self.filelist = ScrolledText(self.listFrame, height=20, width=40)
-        self.filelist.pack(side=LEFT)
-        
+        #self.filelist = ScrolledText(self.listFrame, height=20, width=40)
+        self.listScrollbar = Scrollbar(self.listFrame) 
+        self.filelist = Canvas(self.listFrame, yscrollcommand=self.listScrollbar.set, height=30, width=60)
+
+    def play_file(self, file):
+        print(file)
+        is_music_file = os.path.exists("audio/music/" + file)
+        print(is_music_file)
+        if sys.platform == "win32":
+            if music_file:
+                os.startfile("\\audio\\music\\" + file)
+            else:
+                os.startfile("\\audio\\speech\\" + file)
+        else:
+            opener = "open" if sys.platform == "darwin" else "xdg-open"
+            if is_music_file:
+                subprocess.call([opener, "audio/music/" + file])
+            else:
+                subprocess.call([opener, "audio/speech/" + file])
+           
+
     def fileSort(self, num):
         parts = self.nums.split(num)
         parts[1::2] = map(int, parts[1::2])
         return parts
 
-    def listbar_helper(self, fileListP, startPoint):
+    def filelist_build_helper(self, fileListP, startPoint):
         for i in range(len(fileListP)):
             self.modelBooleans.append(IntVar(0))
-            cb = Checkbutton(self.filelist, text=fileListP[i], variable=self.modelBooleans[i + startPoint], onvalue=1, offvalue=0)
-            self.filelist.window_create('end', window=cb)
-            self.filelist.insert('end', '\n')
+            link = Button(self.filelist, bg="blue")
+            handler=lambda f=fileListP[i]: self.play_file(f)
+            link.configure(command=handler)
+            link.pack(side=LEFT, expand=YES)
+            cb = Checkbutton(link, text=fileListP[i], variable=self.modelBooleans[i + startPoint], onvalue=1, offvalue=0)
+            cb.pack(side=LEFT, expand=YES)
+            
+            self.filelist.create_window(
+                0,
+                (i + startPoint) * self.ymax,
+                anchor=NW,
+                window=link, 
+                width=self.xmax, 
+                height=self.ymax)
+            
 
-    def build_listbar(self):
-        self.listbar_helper(self.speechFiles, 0)
-        self.listbar_helper(self.musicFiles, len(self.speechFiles))
-        # for i in range(len(self.speechFiles)):
-        #     #self.filelist.insert(i, self.speechFiles[i])
-        #     self.modelBooleans.append(IntVar(0))
-        #     cb = Checkbutton(self.filelist, text=self.speechFiles[i], variable=self.modelBooleans[i], onvalue=1, offvalue=0)
-        #     self.filelist.window_create('end', window=cb)
-        #     self.filelist.insert('end', '\n')
-        # for j in range(len(self.musicFiles)):
-        #     #self.filelist.insert(j + len(self.speechFiles), self.musicFiles[j])
-        #     cb = Checkbutton(self.filelist, text=self.musicFiles[j], variable=self.modelBooleans[j], onvalue=1, offvalue=0)
-        #     self.filelist.window_create('end', window=cb)
-        #     self.filelist.insert('end', '\n')
-        self.filelist.pack(side=LEFT, fill=BOTH)
+    def build_filelist(self):
+        filecount = len(self.musicFiles) + len(self.speechFiles)
+        fullsize = (0, 0, (self.xmax), (self.ymax * filecount))
+        
+        # Initialize the canvas with dimensions equal to the number of files
+        self.filelist.delete(ALL)
+        self.filelist.config( 
+            width=self.xmax + 50,
+            height=self.ymax * filecount /2,
+            yscrollcommand=self.listScrollbar.set,
+            scrollregion=fullsize)
+        self.filelist.pack(side=LEFT)
         self.listScrollbar.config(command=self.filelist.yview)
+        self.listScrollbar.pack(side=LEFT, fill=Y)
+
+
+        self.filelist_build_helper(self.speechFiles, 0)
+        self.filelist_build_helper(self.musicFiles, len(self.speechFiles))
 
     def get_audio_files(self):
         self.filelist.delete("1.0","end")
@@ -94,7 +128,7 @@ class FileViewer(Frame):
             self.speechFiles.append(ext)
         print(self.musicFiles)
         print(self.speechFiles)
-        self.build_listbar()
+        self.build_filelist()
         return
         
 
