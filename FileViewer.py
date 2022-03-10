473c1afe3d8fb8ee32eaadfc698f3bf94a362863
Conor McIntosh <mcintc2@uw.edu>
2022-03-10 13:58:59 -0800

    -Play button appears beside file checkbox -Re-added "Play" text to play button -Changed check box text to include either music or speech folder name -Added computational capabilities --Zero crossing, spectral centroids, and chroma frequencies -TODO: need to only run computation on the files with check box toggled
    Co-authored-by: michzelinger <zelinger.mich@gmail.com>
    Co-authored-by: Benjamin Olson <BenjaminOlsonCS@users.noreply.github.com>
    Co-authored-by: kangyul <kangyul@users.noreply.github.com>

--- FileViewer.py
+++ FileViewer.py
@@ -8,6 +8,8 @@ import glob, math, os, re, sys, subprocess, statistics
 from tkinter.scrolledtext import ScrolledText
 from tkinter import filedialog
 from typing import IO
+import librosa
+
 
 class FileViewer(Frame):
 
@@ -16,6 +18,7 @@ class FileViewer(Frame):
         self.master = master
         self.speechFiles = [] 
         self.musicFiles = []
+        self.processedAudioData = []
         self.resultWin = resultWin
         self.nums = re.compile(r'(\d+)')
         self.modelBooleans = []
@@ -51,26 +54,37 @@ class FileViewer(Frame):
         l1.pack(side=LEFT)
 
         #Create File Listbox
-        #self.filelist = Listbox(self.listFrame, yscrollcommand=self.listScrollbar.set, selectmode=BROWSE, height=20, width=40)
-        #self.filelist = ScrolledText(self.listFrame, height=20, width=40)
         self.listScrollbar = Scrollbar(self.listFrame) 
         self.filelist = Canvas(self.listFrame, yscrollcommand=self.listScrollbar.set, height=30, width=60)
 
+    def comp_audio_helper(self, fileListP):
+        returnTuples = []
+        for i in range(len(fileListP)):
+            #check to make sure file's checkbox is checked
+            x, sr = librosa.load(fileListP[i])
+            zero_crossings = librosa.zero_crossings(x)
+            spectral_centroids = librosa.feature.spectral_centroid(x, sr=sr)[0]
+            chromagram = librosa.feature.chroma_stft(x, sr=sr)
+            if "audio/music/" in fileListP[i]:
+                self.processedAudioData.append((fileListP[i], zero_crossings, spectral_centroids, chromagram, "yes"))
+            else:
+                self.processedAudioData.append((fileListP[i], zero_crossings, spectral_centroids, chromagram, "no"))
+        return returnTuples
+
+    def comp_audio(self):
+        self.processedAudioData.clear()
+        processedMusicFiles = self.comp_audio_helper(self.musicFiles)
+        processedSpeechFiles = self.comp_audio_helper(self.speechFiles)
+        #print(self.processedAudioData)
+
     def play_file(self, file):
         print(file)
-        is_music_file = os.path.exists("audio/music/" + file)
-        print(is_music_file)
         if sys.platform == "win32":
             if music_file:
-                os.startfile("\\audio\\music\\" + file)
-            else:
-                os.startfile("\\audio\\speech\\" + file)
+                os.startfile("\\file\\")
         else:
             opener = "open" if sys.platform == "darwin" else "xdg-open"
-            if is_music_file:
-                subprocess.call([opener, "audio/music/" + file])
-            else:
-                subprocess.call([opener, "audio/speech/" + file])
+            subprocess.call([opener, file])
            
 
     def fileSort(self, num):
@@ -81,18 +95,24 @@ class FileViewer(Frame):
     def filelist_build_helper(self, fileListP, startPoint):
         for i in range(len(fileListP)):
             self.modelBooleans.append(IntVar(0))
-            link = Button(self.filelist, bg="blue")
+            link = Button(self.filelist, bg = "light green", text='Play')
             handler=lambda f=fileListP[i]: self.play_file(f)
             link.configure(command=handler)
             link.pack(side=LEFT, expand=YES)
-            cb = Checkbutton(link, text=fileListP[i], variable=self.modelBooleans[i + startPoint], onvalue=1, offvalue=0)
+            self.filelist.create_window(
+                150,
+                (i + startPoint) * self.ymax,
+                anchor=NW,
+                window=link, 
+                width=50, 
+                height=self.ymax)
+            cb = Checkbutton(self.filelist, bg='light blue', text=fileListP[i].replace("audio/", ""), variable=self.modelBooleans[i + startPoint], onvalue=1, offvalue=0)
             cb.pack(side=LEFT, expand=YES)
-            
             self.filelist.create_window(
                 0,
                 (i + startPoint) * self.ymax,
                 anchor=NW,
-                window=link, 
+                window=cb, 
                 width=self.xmax, 
                 height=self.ymax)
             
@@ -115,17 +135,18 @@ class FileViewer(Frame):
 
         self.filelist_build_helper(self.speechFiles, 0)
         self.filelist_build_helper(self.musicFiles, len(self.speechFiles))
-
+    
+    #Grab the audio files from the audio directory in the same space as the FileViewer
     def get_audio_files(self):
         self.filelist.delete("1.0","end")
         self.musicFiles.clear()
         self.speechFiles.clear()
         for infile in sorted(glob.glob('audio/music/*.wav'), key=self.fileSort):
             file, ext = os.path.split(infile)
-            self.musicFiles.append(ext)
+            self.musicFiles.append('audio/music/' + ext)
         for infile in sorted(glob.glob('audio/speech/*.wav'), key=self.fileSort):
             file, ext = os.path.split(infile)
-            self.speechFiles.append(ext)
+            self.speechFiles.append('audio/speech/' + ext)
         print(self.musicFiles)
         print(self.speechFiles)
         self.build_filelist()
